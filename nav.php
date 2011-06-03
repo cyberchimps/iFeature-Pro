c221e7af47da2f55d870bb2f8e43bd8bf707758b
Tyler Cunningham <tyler@cyberchimps.com>
2011-06-02 23:33:41 -0700

    first commit

---
+++ nav.php
@@ -0,0 +1,49 @@
+<?php
+/*
+
+	Section: Navigation
+	Author: Tyler Cunningham
+	Description: Creates site navigation
+	Version: 0.1
+	
+*/
+
+	$options = get_option('ifeature') ;  
+	$homeimage		= $options['if_menuicon'];
+
+?>
+
+<?php $options = get_option('ifeature') ;  
+				if ($options['if_menu_color'] == "")
+					$menucolor = 'Grey';
+				else
+				$menucolor = $options[('if_menu_color')]; ?>
+				
+				<div style ="height: 38px;width: 980px;display: block; background: url(<?php bloginfo('template_url'); ?>/images/menu/<?php echo $menucolor?>.png) no-repeat left top">
+
+<div id="navcontainer">
+
+<?php if ($homeimage == '' ):?>
+<div id="homebutton"><a href="<?php echo home_url(); ?>/"><img src="<?php echo get_template_directory_uri(); ?>/images/menu/home.png?>" alt="Home" /></a></div>
+<?php endif;?>
+<?php if ($homeimage != '' ):?>
+<div id="homebutton"><a href="<?php echo home_url(); ?>/"><img src="<?php echo $homeimage?>" alt="Home" /></a></div>
+<?php endif;?>
+    <div id="sfwrapper">
+        <?php wp_nav_menu( array(
+	    'theme_location' => 'header-menu', // Setting up the location for the main-menu, Main Navigation.
+	    'menu_class' => 'sf-menu', //Adding the class for dropdowns
+	    'container_id' => 'navwrap', //Add CSS ID to the containter that wraps the menu.
+	    'fallback_cb' => 'menu_fallback', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function, which displays the pages of your blog.
+	    )
+	);
+    ?>
+    </div>
+<div id="searchbar">
+<?php get_search_form(); ?>
+</div>
+
+</div><!--end navcontainer-->
+
+</div>    
+<!--end nav.php-->
