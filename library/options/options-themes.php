1895dc11a06185abf9498768f82ea67ab70702c6
Tyler Cunningham <Dillinger@Tyler-Cunninghams-iMac.local>
2011-07-26 18:40:08 -0700

    Fixed CyberChimps Themes page bug

--- library/options/options-themes.php
+++ library/options/options-themes.php
@@ -1,5 +1,4 @@
-<?php   
-
+<?php
 /* 
 	Options	Themes
 	Author: Tyler Cunningham
@@ -9,30 +8,22 @@
 	
 */
 
+add_action('admin_menu', 'cyberchimps_themes_add_menu');
 
- // add the CyberChimps theme page
-	add_action('admin_menu', 'cyberchimps_themes_page');
-	
-	function cyberchimps_themes_page() {
-		add_theme_page('CyberChimps Theme Page', 'CyberChimps Themes', 'manage_options', 'themes', 'cyberchimps_theme_page');
-	}
- 
- // display the CyberChimps theme page
-	function cyberchimps_theme_page() {
-		
-		$root = get_template_directory_uri(); 
+
+// Add sub page to the Settings Menu
+function cyberchimps_themes_add_menu() {
+	add_theme_page('CyberChimps Themes Page', 'CyberChimps Themes', 'administrator', 'themes', 'cyberchimps_themes_page');
+}
+
+// Display the admin options page
+function cyberchimps_themes_page() {
 ?>
-	<div>
+	<div class="wrap">
 		<h2>CyberChimps Themes</h2>
+		Some optional text here explaining the overall purpose of the options and what they relate to etc.
+		<form action="options.php" method="post">
 		
-		IFEETURE PRO<br /> <br />
-		
-		6 MILLION SANDWICHES
-
 	</div>
-
 <?php
-
-}?>
-
-
+}
