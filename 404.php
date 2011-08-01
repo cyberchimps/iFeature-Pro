196d77916e0d1f1dffcac7a2f5d489d120e974da
Tyler Cunningham <Dillinger@Tyler-Cunninghams-iMac.local>
2011-07-31 21:47:15 -0700

    404 image

--- 404.php
+++ 404.php
@@ -21,7 +21,7 @@
 		<div class="content_padding">
 
 			<div class="error">Error 404<br />
-				These aren't the droids you're looking for.
+				<center></center><img src="<?php echo get_template_directory_uri() ;?>/images/confusedchimp.png" height="400" width="400" /></center>
 			</div>
 		
 		</div><!--end content_padding-->
