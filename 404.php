8669af4a0347d8747d357ea9147e851ee63c2f07
Tyler Cunningham <tyler@cyberchimps.com>
2011-10-19 16:26:18 -0700

    more 404

--- 404.php
+++ 404.php
@@ -17,8 +17,19 @@
 <div id="content_wrap">
 	<div id="content_left">
 		<div class="content_padding">
-      
-      <?php chimps_404(); ?>
+		
+			<!-- Begin @Core before_404 hook content-->
+      			<?php chimps_before_404(); ?>
+      		<!-- Begin @Core before_404 hook content-->
+		
+      		<!-- Begin @Core 404 hook content-->
+      			<?php chimps_404(); ?>
+      		<!-- Begin @Core 404 hook content-->
+      		
+      		<!-- Begin @Core after_404 hook content-->
+      			<?php chimps_after_404(); ?>
+      		<!-- Begin @Core after_404 hook content-->
+      		
 		</div><!--end content_padding-->
 		
 	</div><!--end content_left-->
