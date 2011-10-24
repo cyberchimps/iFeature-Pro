3f1b8f5a554d6e1aef750a425146c34697e20e53
Tyler Cunningham <tyler@cyberchimps.com>
2011-10-24 15:17:42 -0700

    moved breadcrumbs to entry-actions, not sure why I moved it to core functions

--- core/actions/search-actions.php
+++ core/actions/search-actions.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* ___ actions used by the CyberChimps Core Framework 
+* Search actions used by the CyberChimps Core Framework 
 *
 * Author: Tyler Cunningham
 * Copyright: © 2011
@@ -16,13 +16,54 @@
 */
 
 /**
-* Core ___ actions
+* Core search actions
 */
+add_action( 'chimps_search', 'chimps_search_content' );
 
 /**
-* Core ___ functions
+* Search results output
+*
+* @since 1.0
 */
+function chimps_search_content() { ?>
+	<div id="content_left">
+		
+		<div class="content_padding">
 
+		<?php if (have_posts()) : ?>
+
+		<h2><font size="5"><?php printf( __( 'Search Results For: %s' ), '<span>' . get_search_query() . '</span>' ); ?></font></h2><br />
+
+		<?php while (have_posts()) : the_post(); ?>
+		
+		<div class="post_container">
+
+			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+				<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+				<?php get_template_part('meta', 'search' ); ?>
+
+				<div class="entry">
+
+					<?php the_excerpt(); ?>
+
+				</div>
+
+			</div>
+
+	</div><!--end post_container-->
+		<?php endwhile; ?>
+
+		<?php chimps_pagination(); ?>
+
+	<?php else : ?>
+
+		<h2>No posts found.</h2>
+
+	<?php endif; ?>
+		</div><!--end content_padding-->
+	</div><!--end content_left--><?php
+}
 
 /**
 * End
--- search.php
+++ search.php
@@ -8,51 +8,30 @@
 	Copyright (C) 2011 CyberChimps
 */
 
-get_header(); ?>
+get_header(); 
+
+?>
 
 <div id="content_wrap">
 
-<?php if (function_exists('chimps_breadcrumbs')) chimps_breadcrumbs(); ?>
-
-	<div id="content_left">
-		
-		<div class="content_padding">
-
-		<?php if (have_posts()) : ?>
-
-		<h2><font size="5"><?php printf( __( 'Search Results For: %s' ), '<span>' . get_search_query() . '</span>' ); ?></font></h2><br />
-
-		<?php while (have_posts()) : the_post(); ?>
-		
-		<div class="post_container">
-
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-				<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-				<?php get_template_part('meta', 'search' ); ?>
-
-				<div class="entry">
-
-					<?php the_excerpt(); ?>
-
-				</div>
-
-			</div>
-
-	</div><!--end post_container-->
-		<?php endwhile; ?>
-
-		<?php get_template_part('pagination', 'search'); ?>
-
-	<?php else : ?>
-
-		<h2>No posts found.</h2>
-
-	<?php endif; ?>
-		</div><!--end content_padding-->
-	</div><!--end content_left-->
-
+	<!-- Begin @Core before_entry hook -->
+		<?php chimps_before_entry(); ?>
+	<!-- End @Core before_entry hook -->
+	
+	<!-- Begin @Core before_search hook -->
+		<?php chimps_before_search(); ?>
+	<!-- End @Core before_search hook -->
+	
+	<!-- Begin @Core search hook -->
+		<?php chimps_search(); ?>
+	<!-- End @Core search hook -->
+	
+	<!-- Begin @Core after_search hook -->
+		<?php chimps_after_search(); ?>
+	<!-- End @Core after_search hook -->
+	
 	<div id="sidebar_right"><?php get_sidebar(); ?></div>
 </div><!--end content_wrap-->
 <div class="clear"></div>
+
 <?php get_footer(); ?>
