a0ef1f43d57d15e08e4e35a12aa4db5e55dc2828
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-07 22:20:12 -0800

    working on color array

--- core/classy-options/classy-options-init.php
+++ core/classy-options/classy-options-init.php
@@ -37,7 +37,7 @@ $options
 	->section("Design")
 		->select($themeslug."_font", "Choose a Font", array( 'options' => array("Arial" => "Arial (default)", "Courier New" => "Courier New", "Georgia" => "Georgia", "Helvetica" => "Helvetica", "Lucida Grande" => "Lucida Grande", "Tahoma" => "Tahoma", "Times New Roman" => "Times New Roman", "Verdana" => "Verdana", "Actor" => "Actor", "Coda" => "Coda", "Maven+Pro" => "Maven Pro", "Metrophobic" => "Metrophobic", "News+Cycle" => "News Cycle", "Nobile" => "Nobile", "Tenor+Sans" => "Tenor Sans", "Quicksand" => "Quicksand", "Ubuntu" => "Ubuntu")))
 		->text($themeslug."_custom_font", "Enter a Custom Font")
-		->select($themeslug."_color_scheme", "Select a Color Scheme", array( 'options' => array("key1" => "Blue", "key2" => "Black")))
+		->select($themeslug."_color_scheme", "Select a Color Scheme", array( 'options' => array("blue" => "Blue (default)", "black" => "Black", "green" => "Green", "orange" => "Orange",)))
 		->color($themeslug."_menu_link_color", "Menu Link Color")
 		->color($themeslug."_site_title_color", "Site Title Color")
 		->color($themeslug."_site_description_color", "Site Description Color")
