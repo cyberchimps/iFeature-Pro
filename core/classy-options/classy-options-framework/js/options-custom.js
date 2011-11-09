07a866cd5910d30012ce78d24ef8caca47089ca3
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-11-10 04:50:22 +0530

    Fade toggle instead of toggle.

--- core/classy-options/classy-options-framework/js/options-custom.js
+++ core/classy-options/classy-options-framework/js/options-custom.js
@@ -146,7 +146,7 @@ jQuery(document).ready(function($) {
 		 		
 	$('.subsection-items').hide();
 	$('.subsection > h3').click(function() {
-		$(this).siblings('div').toggle();
+		$(this).siblings('div').fadeToggle();
 	});
 });	
 
