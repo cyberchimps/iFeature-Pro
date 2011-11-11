8c3b9091b817d11b7effa08091560a31103fc1af
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-11-11 11:06:45 +0530

    Almost done with social checkboxes and texts.

--- core/classy-options/classy-options-framework/js/options-custom.js
+++ core/classy-options/classy-options-framework/js/options-custom.js
@@ -182,5 +182,20 @@ jQuery(document).ready(function($) {
       post.show(); custom.hide();
     }
   }).change();
+
+  $("#of-option-social div.section-text").each(function(){
+    var $this = $(this), $next = $(this).next();
+    $this.find(".controls").css({float: 'left', clear: 'both'});
+    $next.find(".controls").css({float: 'right', width: 80});
+    $next.hide();
+    $this.find('.option').before($next.find(".option"));
+    $this.find("input[type='checkbox']").change(function() {
+      if($(this).is(":checked")) {
+        $(this).closest('.option').next().show();
+      } else {
+        $(this).closest('.option').next().hide();
+      }
+    }).change();
+  });
 });	
 
