8702b953c9c488d72d4eeecbd0d84c94501b0740
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-14 16:34:55 -0800

    re-formated theme options

--- core/core-init.php
+++ core/core-init.php
@@ -38,7 +38,7 @@ require_once ( get_template_directory() . '/core/pro/pro-init.php' );
 
 //Options
 //require_once ( get_template_directory() . '/core/options/options-init.php' );
-//require_once ( get_template_directory() . '/core/options/options-themes.php' );
+require_once ( get_template_directory() . '/core/options/options-themes.php' );
 
 require_once ( get_template_directory() . '/core/classy-options/classy-options-init.php' );
 require_once ( get_template_directory() . '/core/classy-options/options-functions.php' );
