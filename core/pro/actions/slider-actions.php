f5aeb6c106e75af06ba603937499e95911f37775
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-16 10:33:22 -0800

    added height to slider wrapper on blog page, need to finalize styling so I can build functionality to support the extra height

--- core/pro/actions/slider-actions.php
+++ core/pro/actions/slider-actions.php
@@ -55,7 +55,7 @@ function chimps_blog_slider_content() {
 	
 
    
-
+echo "<div id='slider-wrapper'>";
 	
 /* End define variables. */	
 
@@ -436,7 +436,7 @@ To create a Custom Slide please go to the Custom Slides tab in WP-Admin. Once yo
 <!-- Apply slider CSS based on user settings -->
 
 	<style type="text/css" media="screen">
-		#slider-wrapper { width: <?php echo $csWidth ?>px; height: <?php echo $height ?>px; margin: auto; margin-bottom: 50px;}
+		#slider-wrapper { width: <?php echo $csWidth ?>px;  margin: auto; margin-bottom: 50px;}
 		#slider { width: <?php echo $csWidth ?>px; height: <?php echo $height ?>px; margin: auto; }
 	</style>
 
@@ -518,6 +518,7 @@ echo $out;
 
 /* END */ 
 
+echo "</div>";
 
 }
 
