3fd56f4840938bddded52c2ae2aaaad3701004c4
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-16 11:28:36 -0800

    fixed date archive titles/bug

--- core/actions/archive-actions.php
+++ core/actions/archive-actions.php
@@ -30,7 +30,7 @@ function chimps_archive_page_title() {
 	global $post; ?>
 	
 
- 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+ 	
 
 			<?php if (is_category()) { ?>
 				<h2><?php printf( __( 'Archive for the &#8216;', 'core' )); ?><?php single_cat_title(); ?><?php printf( __( '&#8217; Category:', 'core' )); ?></h2><br />
@@ -39,10 +39,10 @@ function chimps_archive_page_title() {
 				<h2><?php printf( __( 'Posts Tagged &#8216;', 'core' )); ?><?php single_tag_title(); ?><?php printf( __( '&#8217;:', 'core' )); ?></h2><br />
 
 			<?php } elseif (is_day()) { ?>
-				<h2><?php printf( __( 'Archive for:', 'core' )); ?><?php the_time('F jS, Y'); ?>:</h2><br />
+				<h2><?php printf( __( 'Archive for', 'core' )); ?> <?php the_time('F jS, Y'); ?>:</h2><br />
 
 			<?php } elseif (is_month()) { ?>
-				<h2><?php printf( __( 'Archive for:', 'core' )); ?><?php the_time('F, Y'); ?>:</h2><br />
+				<h2><?php printf( __( 'Archive for', 'core' )); ?> <?php the_time('F, Y'); ?>:</h2><br />
 
 			<?php } elseif (is_year()) { ?>
 				<h2 class="pagetitle"><?php printf( __( 'Archive for:', 'core' )); ?> <?php the_time('Y'); ?>:</h2><br />
