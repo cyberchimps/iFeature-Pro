96bcb0d4841e754f64d3e68503fddeb413aae9a1
Trent Lapinski <cyberchimpspro@gmail.com>
2011-11-21 13:20:19 -0800

    menu added to options

--- core/classy-options/classy-options-framework/classy-options.php
+++ core/classy-options/classy-options-framework/classy-options.php
@@ -78,6 +78,21 @@ class ClassyOptions {
 				<h2><?php esc_html_e($this->name); ?></h2>
 				</div>
 				<div class="clear"></div>
+					<p class="submit" style="clear:left;float: right;">
+						<input type="submit" class="button-primary" value="Save Settings" />   
+					</p>
+				<div class="menu">
+					<ul>
+						<li><a href="http://cyberchimps.com/support" target="_blank"><?php printf( __( 'Support', 'ifeature' )); ?></a></li>
+						<li><a href="http://cyberchimps.com/ifeature-free/docs" target="_blank"> <?php printf( __( 'Documentation', 'ifeature' )); ?></a></li>
+						<li><a href="http://cyberchimps.com/forum/" target="_blank"><?php printf( __( 'Forum', 'ifeature' )); ?></a></li>
+						<li><a href="http://twitter.com/#!/cyberchimps" target="_blank"><?php printf( __( 'Twitter', 'ifeature' ));?></a></li>
+						<li><a href="http://facebook.com/cyberchimps/" target="_blank"><?php printf( __( 'Facebook', 'ifeature' ));?></a></li>
+						<li><a href="http://cyberchimps.com/store/" target="_blank"><?php printf( __( 'CyberChimps Store', 'ifeature' )); ?></a></li>
+						<li><a href="http://cyberchimpspro.com/" target="_blank"><?php printf( __( 'CyberChimps Pro', 'ifeature' )); ?></a></li>
+					</ul>
+				</div>
+
 			</div>
 			<div id="main">
 				<?php $return = $this->fields(); ?>
