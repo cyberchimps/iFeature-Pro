3bbd907203e7e070e74eb92a7d32bbfe3ca3267d
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-22 10:37:25 -0800

    small tweak to upload case

--- core/classy-options/classy-options-framework/classy-options.php
+++ core/classy-options/classy-options-framework/classy-options.php
@@ -375,7 +375,7 @@ class ClassyOptions {
 				if(isset($val['url'])) {
 					$output .= "Preview: " . "<img src='{$val['url']}' width='350px' height='50px'/><br/>";
 				}
-				$output .= " URL <input type='text' name='{$value['id']}_text' size='72' value='" . (isset($val['url']) ? $val['url'] : "") . "'/>";
+				$output .= " &nbsp;&nbsp;&nbsp;&nbsp; URL <input type='text' name='{$value['id']}_text' size='72' value='" . (isset($val['url']) ? $val['url'] : "") . "'/>";
 				$output .= " or upload File: <input type='file' id='{$value['id']}' name='{$value['id']}'>";
 			break;
 			
