1d25bf370e60afe21110c3ea809df56857cb7e47
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-21 17:00:12 -0800

    comment stuff

--- core/actions/comments-actions.php
+++ core/actions/comments-actions.php
@@ -54,7 +54,7 @@ function chimps_comments_loop() {
 		</div>
 
 		<ol class="commentlist">
-			<?php wp_list_comments(); ?>
+			<?php wp_list_comments('type=comment&callback=mytheme_comment'); ?>
 		</ol>
 
 		<div class="navigation">
