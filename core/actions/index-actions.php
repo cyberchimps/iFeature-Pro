9768379ad27fb618942443c0e2ea3e1ec93c2b7c
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-23 06:32:45 -0800

    hide post formats icons option

--- core/actions/index-actions.php
+++ core/actions/index-actions.php
@@ -127,6 +127,7 @@ function chimps_index_loop_content($content) { ?>
 	 <?
 
 	global $options, $themeslug, $post; //call globals
+	
 		
 	if (get_post_format() == '') {
 		$format = "default";
@@ -138,9 +139,11 @@ function chimps_index_loop_content($content) { ?>
 		
 		<?php ob_start(); ?>
 			
+			<?php if ($options->get($themeslug.'_post_formats') == '1') : ?>
 			<div class="postformats"><!--begin format icon-->
 				<img src="<?php echo get_template_directory_uri(); ?>/images/formats/<?php echo $format ;?>.png" />
 			</div><!--end format-icon-->
+			<?php endif; ?>
 				<h2 class="posts_title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 					<!--Call @Core Meta hook-->
 			<?php chimps_post_byline(); ?>
