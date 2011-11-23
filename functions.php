3652202cbecb1622a9062da34fb5364aa8b85201
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-22 18:32:44 -0800

    cleaning up functions file

--- functions.php
+++ functions.php
@@ -17,9 +17,10 @@
 	$themeslug = 'if';
 	$root = get_template_directory_uri(); 
 	
-	
+/* Custom comment callback function */
+
 function mytheme_comment($comment, $args, $depth) {
-   $GLOBALS['comment'] = $comment; ?>
+   	$GLOBALS['comment'] = $comment; ?>
    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
      <div id="comment-<?php comment_ID(); ?>">
       <div class="comment-author vcard">
@@ -41,7 +42,7 @@ function mytheme_comment($comment, $args, $depth) {
       </div>
      </div>
 <?php
-        }
+}
 	
 /* Localization */
 	    
@@ -94,49 +95,48 @@ add_filter('excerpt_length', 'new_excerpt_length');
 /* Add auto-feed links support. */	
 add_theme_support('automatic-feed-links');
 	
-/* Add post-thumb support. */
-
+/* Sets featured image size based on theme options. */
 function init_featured_image() {	
-if ( function_exists( 'add_theme_support' ) ) {
+	if ( function_exists( 'add_theme_support' ) ) {
 
- global $themename, $themeslug, $options;
+ 	global $themename, $themeslug, $options;
 	
 		if ($options->get($themeslug.'_featured_image_height') == '') {
 			$featureheight = '100';
-	}		
+		}		
 	
-	else {
-		$featureheight = $options->get($themeslug.'_featured_image_height'); 
+		else {
+			$featureheight = $options->get($themeslug.'_featured_image_height'); 
 		
-	}
+		}
 	
 		if ($options->get($themeslug.'_featured_image_width') == "") {
 			$featurewidth = '100';
-	}		
+		}		
 	
-	else {
-		$featurewidth = $options->get($themeslug.'_featured_image_width'); 
-	} 
+		else {
+			$featurewidth = $options->get($themeslug.'_featured_image_width'); 
+		} 
 	 
 	set_post_thumbnail_size( $featureheight, $featurewidth, true );
-}	
+	}	
 }
 add_action( 'init', 'init_featured_image', 11);	
 
+// This theme allows users to set post featured images.
 add_theme_support( 'post-thumbnails' );
-// This theme allows users to set a custom background
-
 
 // This theme styles the visual editor with editor-style.css to match the theme style.
 add_editor_style();
 
 // Load jQuery
+
 if ( !is_admin() ) {
- 
-	   wp_enqueue_script('jquery');
-  }
-
-
+	function ifeature_frontend_scripts() {
+		wp_enqueue_script('jquery');
+	}
+add_action('wp_enqueue_scripts', 'ifeature_frontend_scripts');
+}
 
 /**
 * Attach CSS3PIE behavior to elements
@@ -156,9 +156,6 @@ function render_ie_pie() { ?>
 
 add_action('wp_head', 'render_ie_pie', 8);
 	
-
-
-
 // Create custom post type for Slider
 
 add_action( 'init', 'create_post_type' );
@@ -297,7 +294,6 @@ function carousel(){
 }
 add_action('wp_head', 'carousel');
 
-
 // + 1 Button 
 
 function plusone(){
@@ -325,42 +321,18 @@ function typekit_support() {
 }
 add_action('wp_head', 'typekit_support');
 
-
-// Register superfish scripts
+// Register menu names
 	
-function add_scripts() {
- 
-    if (!is_admin()) { // Add the scripts, but not to the wp-admin section.
-    // Adjust the below path to where scripts dir is, if you must.
-    $scriptdir = get_template_directory_uri() ."/library/sf/";
- 
-    // Register the Superfish javascript file
-    wp_register_script( 'superfish', $scriptdir.'sf.js', false, '1.4.8');
-    wp_register_script( 'sf-menu', $scriptdir.'sf-menu.js');
-    // Now the superfish CSS
-   
-    //load the scripts and style.
-	wp_enqueue_style('superfish-css');
-    wp_enqueue_script('superfish');
-    wp_enqueue_script('sf-menu');
-    } // end the !is_admin function
-} //end add_our_scripts function
- 
-//Add our function to the wp_head. You can also use wp_print_scripts.
-add_action( 'wp_head', 'add_scripts',0);
-	
-	// Register menu names
-	
-	function register_menus() {
+function register_menus() {
 	register_nav_menus(
 	array( 'header-menu' => __( 'Header Menu' ), 'footer-menu' => __( 'Footer Menu' ))
   );
 }
 	add_action( 'init', 'register_menus' );
 	
-	// Menu fallback
+// Menu fallback
 	
-	function menu_fallback() {
+function menu_fallback() {
 	global $post; ?>
 	
 	<ul id="menu-nav" class="sf-menu">
