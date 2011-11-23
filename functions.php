e1e2df651f2482f9c604c1d73203286c4a7f92d0
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-23 14:25:44 -0800

    quick fix other call was wonky

--- functions.php
+++ functions.php
@@ -371,16 +371,12 @@ add_action('wp_head', 'typekit_support');
 
 // Register jQuery
 	
-function my_scripts_method() {
-
 // Load jQuery
-
-	 wp_deregister_script('jquery');
-	 wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"), false); //temp fix
-     wp_enqueue_script( 'jquery' );
-}    
- 
-add_action('wp_enqueue_scripts', 'my_scripts_method');
+	if ( !is_admin() ) {
+	   wp_deregister_script('jquery');
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"), false);
+	   wp_enqueue_script('jquery');
+	}
 	
 	// Register menu names
 	
