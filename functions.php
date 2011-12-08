eac4edb026ee2c303d9950d896f9ec3730ceb323
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-08 15:23:29 -0800

    last push for 3.0 branch, going to move this over to the master branch

--- functions.php
+++ functions.php
@@ -383,10 +383,12 @@ add_action('wp_head', 'typekit_support');
 // Load jQuery
 function if_jquery() {
 	if ( !is_admin() ) {
+	   wp_deregister_script('jquery');
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"), false);
 	   wp_enqueue_script('jquery');
 	}
 }
-add_action('wp_enqueue_scripts', 'if_jquery');
+add_action('wp_enqueue_scripts', 'if_jquery');	
 	
 	// Register menu names
 	
