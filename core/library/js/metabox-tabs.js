31667dd09a49943905af029ba65e5756d8e59af1
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-09 11:09:05 -0800

    reverting to old meta box code, moved development code to 3.0 branch

--- core/library/js/metabox-tabs.js
+++ core/library/js/metabox-tabs.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
 	// tab between them
 	jQuery('.metabox-tabs li a').each(function(i) {
 		var thisTab = jQuery(this).parent().attr('class').replace(/active /, '');
@@ -23,36 +23,4 @@ jQuery(document).ready(function($) {
 
 	jQuery('.heading').hide();
 	jQuery('.metabox-tabs').show();
-
-	$(".subsection-items").hide();
-	$(".subsection > h4").click(function() {
-		var $this = $(this);
-		$this.find("span.minus").removeClass('minus');
-		if($this.siblings('div').is(":visible")) {
-			$this.siblings('div').slideUp();
-		} else {
-			$this.siblings('div').slideDown();
-			$this.find("span").addClass('minus');
-		}
-	});
-
-	// show by default
-	$("#subsection-Page-Options .subsection-items").show();
-	var page_subsection_map = {
-		page_slider: "subsection-iFeature-Pro-Slider-Options",
-		callout_section: "subsection-Callout-Options",
-		twitterbar_section: "subsection-Twitter-Options",
-		box_section: "subsection-Carousel-Options",
-		carousel_section: "subsection-SEO-Options"
-	};
-	$("#page_section_order").change(function(){
-		var array = $(this).val().split(",");
-		$.each(page_subsection_map, function(key, value) {
-			if($.inArray(key, array) != -1) {
-				$("#" + value).show();
-			} else {
-				$("#" + value).hide();
-			}
-		});
-	}).change();
 });
