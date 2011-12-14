56fbbb00cb94f5ddff9a42683300d4c52a85c4a2
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-14 15:25:48 -0800

    looks pretty good now

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 /*
 	Page
-	Establishes the core iFeature page tempate.
+	Establishes the iFeature Pro page tempate.
 	Version: 3.0
 	Copyright (C) 2011 CyberChimps
 
@@ -16,7 +16,6 @@
 
 /* Define global variables. */
 
-	
 	$size = get_post_meta($post->ID, 'page_slider_size' , true);
 	$page_section_order = get_post_meta($post->ID, 'page_section_order' , true);
 	if(!$page_section_order) {
@@ -25,19 +24,15 @@
 
 /* End define global variables. */
 
-
+/* Set slider hook based on page option */
 if ($size == "0") {
-
-add_action ('chimps_page_slider', 'chimps_page_slider_content' );
-
+	add_action ('chimps_page_slider', 'chimps_page_slider_content' );
 }
 
 if (preg_match("/page_slider/", $page_section_order ) && $size == "1" ) {
-
-add_action ('chimps_page_content_slider', 'chimps_page_slider_content' );
-
+	add_action ('chimps_page_content_slider', 'chimps_page_slider_content' );
 }
-
+/* End set slider hook*/
 ?>
 
 <div class="container_12">
