32db2bff0dfff73ffb31c7af69811115700b2ad3
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-14 15:48:37 -0800

    can't remember

--- core/pro/actions/page-actions.php
+++ core/pro/actions/page-actions.php
@@ -1,9 +1,28 @@
 <?php
+/**
+* Page actions used by the CyberChimps Core Framework Pro Extension
+*
+* Author: Tyler Cunningham
+* Copyright: © 2011
+* {@link http://cyberchimps.com/ CyberChimps LLC}
+*
+* Released under the terms of the GNU General Public License.
+* You should have received a copy of the GNU General Public License,
+* along with this software. In the main directory, see: /licensing/
+* If not, see: {@link http://www.gnu.org/licenses/}.
+*
+* @package Pro
+* @since 1.0
+*/
 
 remove_action('chimps_page_section', 'chimps_page_section_content' );
 add_action('chimps_page_section', 'chimps_pro_page_section_content' );
 
-
+/**
+* Pro page section content.
+*
+* @since 1.0
+*/
 function chimps_pro_page_section_content() { 
 	global $options, $themeslug, $post;
 	
