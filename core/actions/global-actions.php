456a48334afa93e920ad73de870767610f390ab2
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-14 20:55:30 -0800

    starting default/example theme options

--- core/actions/global-actions.php
+++ core/actions/global-actions.php
@@ -36,7 +36,7 @@ function chimps_post_byline_content() {
 		<?php if (($hidden[$themeslug.'_hide_date']) != '0'):?> <?php printf( __( 'Published on', 'core' )); ?> <a href="<?php the_permalink() ?>"><?php the_time('F jS, Y') ?></a><?php endif;?>
 		<?php if (($hidden[$themeslug.'_hide_author']) != '0'):?><?php printf( __( 'by', 'core' )); ?> <?php the_author_posts_link(); ?> <?php endif;?> 
 		<?php if (($hidden[$themeslug.'_hide_categories']) != '0'):?><?php printf( __( 'in', 'core' )); ?> <?php the_category(', ') ?> <?php endif;?>
-		</div> <?php
+	</div> <?php
 }
 
 /**
