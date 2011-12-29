e138966bc2ee8160ba89127f8bb3ade533f2ca2a
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-29 14:00:39 -0800

    some change

--- inc/options-functions.php
+++ inc/options-functions.php
@@ -1,11 +1,18 @@
 <?php
-/* 
-	Options	Functions
-	Author: Tyler Cuningham
-	Establishes the theme options functions.
-	Copyright (C) 2011 CyberChimps
-	Version 2.0
-	
+/**
+* Functions related to the iFeature Theme Options.
+*
+* Author: Tyler Cunningham
+* Copyright: © 2011
+* {@link http://cyberchimps.com/ CyberChimps LLC}
+*
+* Released under the terms of the GNU General Public License.
+* You should have received a copy of the GNU General Public License,
+* along with this software. In the main directory, see: /licensing/
+* If not, see: {@link http://www.gnu.org/licenses/}.
+*
+* @package iFeature
+* @since 3.1
 */
 
 /* Widget Title Background*/
