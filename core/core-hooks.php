e56e9a321789453d7af93ed2b211d2ef24632c8d
Tyler Cunningham <tyler@cyberchimps.com>
2012-01-03 11:07:02 -0800

    moved modified core from free to pro, need to update template files with new hook names

--- core/core-hooks.php
+++ core/core-hooks.php
@@ -15,217 +15,370 @@
 * @since 1.0
 */
 
-/** 
-* 404
+/**
+* Placed before the 404 message content (404.php).
+*
+* @since 1.0
 */
 function chimps_before_404() {
 	do_action('chimps_before_404');
 }
 
+/**
+* 404 page template message content (404.php).
+*
+* @since 1.0
+*/
 function chimps_404() {
 	do_action('chimps_404');
 }
 
+/**
+* Placed after the 404 message content (404.php).
+*
+* @since 1.0
+*/
 function chimps_after_404() {
 	do_action('chimps_after_404');
 }
 
-/** 
-* Archive
+/**
+* Placed before the archive template content (archive.php). 
+*
+* @since 1.0
 */
 function chimps_before_archive() {
 	do_action('chimps_before_archive');
 }
 
-function chimps_archive() {
-	do_action('chimps_archive');
-}
-
+/**
+* Conditionals for various archive page title types (archive.php).
+*
+* @since 1.0
+*/
 function chimps_archive_title() {
 	do_action('chimps_archive_title');
 }
 
+/**
+* Archive template loop content (archive.php).
+*
+* @since 1.0
+*/
+function chimps_archive() {
+	do_action('chimps_archive');
+}
+
+/**
+* Placed after the archive template content (archive.php). 
+*
+* @since 1.0
+*/
 function chimps_after_archive() {
 	do_action('chimps_after_archive');
 }
 
-/** 
-* Comments
+/**
+* Placed after the comment section content (comments.php). 
+*
+* @since 1.0
 */
 function chimps_before_comments() {
 	do_action('chimps_before_comments');
 }
 
+/**
+* Creates the comment section (comments.php). 
+*
+* @since 1.0
+*/
 function chimps_comments() {
 	do_action('chimps_comments');
 }
 
+/**
+* Placed after the comment section (comments.php). 
+*
+* @since 1.0
+*/
 function chimps_after_comments() {
 	do_action('chimps_after_comments');
 }
 
-/** 
-* Content 
+/**
+* For use before main page content. 
+*
+* @since 1.0
 */
-function chimps_before_content() {
-	do_action('chimps_before_content');
+function chimps_before_page_content() {
+	do_action('chimps_before_page_content');
 }
 
-function chimps_after_content() {
-	do_action('chimps_after_content');
-}
-
-
-/** 
-* Index
+/**
+* For use after main page content. 
+*
+* @since 1.0
 */
-
-function chimps_index_after_entry() {
-	do_action('chimps_index_after_entry');
+function chimps_after_page_content() {
+	do_action('chimps_after_page_content');
 }
 
-function chimps_index_entry() {
-	do_action('chimps_index_entry');
+/**
+* Placed after post entry (sets up sidebar). 
+*
+* @since 1.0
+*/
+function chimps_after_entry() {
+	do_action('chimps_after_entry');
 }
 
-function chimps_index_loop() {
-	do_action('chimps_index_loop');
+/**
+* For use before the loop. 
+*
+* @since 1.0
+*/
+function chimps_before_loop() {
+	do_action('chimps_before_loop');
 }
 
-function chimps_index_before_loop() {
-	do_action('chimps_index_before_loop');
+/**
+* The loop. 
+*
+* @since 1.0
+*/
+function chimps_loop() {
+	do_action('chimps_loop');
 }
 
-function chimps_index_after_loop() {
-	do_action('chimps_index_after_loop');
+/**
+* For use after the loop. 
+*
+* @since 1.0
+*/
+function chimps_after_loop() {
+	do_action('chimps_after_loop');
 }
 
-/** 
-* Footer 
+/**
+* For use before the footer content. 
+*
+* @since 1.0
 */
 function chimps_before_footer() {
 	do_action('chimps_before_footer_content');
 }
 
+/**
+* Footer content. 
+*
+* @since 1.0
+*/
 function chimps_footer() {
 	do_action('chimps_footer');
 }
 
-function chimps_afterfooter() { 
-	do_action('chimps_afterfooter');
-}
-
+/**
+* For use after the footer content. 
+*
+* @since 1.0
+*/
 function chimps_after_footer() {
 	do_action('chimps_after_footer_content');
 }
 
-/** 
-* Global 
+/**
+* Contains the secondary footer elements. 
+*
+* @since 1.0
 */
-
-function chimps_meta() {
-	do_action('chimps_meta');
+function chimps_secondary_footer() { 
+	do_action('chimps_secondary_footer');
 }
 
+/**
+* Post byline content. 
+*
+* @since 1.0
+*/
 function chimps_post_byline() {
 	do_action('chimps_post_byline');
 }
 
+/**
+* Calls post tags. 
+*
+* @since 1.0
+*/
 function chimps_post_tags() {
 	do_action('chimps_post_tags');
 }
 
+/**
+* Post pagination. 
+*
+* @since 1.0
+*/
 function chimps_link_pages() {
 	do_action('chimps_link_pages');
 }
 
+/**
+* Creates admin edit link for pages and posts. 
+*
+* @since 1.0
+*/
 function chimps_edit_link() {
 	do_action('chimps_edit_link');
 }
 
-/** 
-* Header 
+/**
+* Contains HTML, title, rel and meta elements. 
+*
+* @since 1.0
+*/
+function chimps_head_tag() {
+	do_action('chimps_head_tag');
+}
+
+/**
+* Placed after closing HEAD tag, contains font function. 
+*
+* @since 1.0
 */
 function chimps_after_head_tag() {
 	do_action('chimps_after_head_tag');
 }
 
+/**
+* For adding content before the main header content. 
+*
+* @since 1.0
+*/
 function chimps_before_header() {
 	do_action('chimps_before_header');
 }
 
-function chimps_head_tag() {
-	do_action('chimps_head_tag');
-}
-
+/**
+* For adding content after the main header content. 
+*
+* @since 1.0
+*/
 function chimps_after_header() {
 	do_action('chimps_after_header');
 }
 
+/**
+* Sitename/logo content. 
+*
+* @since 1.0
+*/
 function chimps_header_sitename() {
 	do_action('chimps_header_sitename');
 }
 
+/**
+* Site description. 
+*
+* @since 1.0
+*/
 function chimps_header_site_description() {
 	do_action('chimps_header_site_description');
 }
 
+/**
+* Header social icon section. 
+*
+* @since 1.0
+*/
 function chimps_header_social_icons() {
 	do_action('chimps_header_social_icons');
 }
 
+/**
+* Site menu. 
+*
+* @since 1.0
+*/
 function chimps_navigation() {
 	do_action('chimps_navigation');
 }
 
-
-
-/** 
-* Pagination 
+/**
+* Index pagination. 
+*
+* @since 1.0
 */
 function chimps_pagination() { 
 	do_action('chimps_pagination');
 }
 
+/**
+* Post page pagination. 
+*
+* @since 1.0
+*/
 function chimps_links_pages() { 
 	do_action('chimps_links_pages');
 }
 
+/**
+* Next/Prev post links for single.php. 
+*
+* @since 1.0
+*/
 function chimps_post_pagination() { 
 	do_action('chimps_post_pagination');
 }
 
-/** 
-* page
+/**
+* Sets up the page section for page.php. 
+*
+* @since 1.0
 */
 function chimps_page_section() {
 	do_action('chimps_page_section');
 }
 
-/** 
-* Search
+/**
+* Placed before the search result content. 
+*
+* @since 1.0
 */
 function chimps_before_search() {
 	do_action('chimps_before_search');
 }
 
+/**
+* Sets up the search result content. 
+*
+* @since 1.0
+*/
 function chimps_search() {
 	do_action('chimps_search');
 }
 
+/**
+* Placed after the search result content. 
+*
+* @since 1.0
+*/
 function chimps_after_search() {
 	do_action('chimps_after_search');
 }
 
-/** 
-* Slider
+/**
+* Generates the lite version of the iFeature slider. 
+*
+* @since 1.0
 */
 function chimps_blog_slider_lite() {
 	do_action('chimps_blog_slider_lite');
 }
 
-/** 
-* Twitterbar Section
+/**
+* Generates the Twitter Bar page element. 
+*
+* @since 1.0
 */
 function chimps_twitterbar_section() {
 	do_action ('chimps_twitterbar_section');
