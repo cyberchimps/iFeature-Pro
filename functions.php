6a6b2e2c151ba3d0c1456238e6ef0940287b0cb6
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-01-25 19:46:24 +0530

    Add javascript for tabs.

--- functions.php
+++ functions.php
@@ -399,6 +399,7 @@ function if_jquery() {
 	   wp_deregister_script('jquery');
 	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"), false);
 	   wp_enqueue_script('jquery');
+	   wp_enqueue_script('jquery-ui-tabs');
 	}
 }
 add_action('wp_enqueue_scripts', 'if_jquery');	
