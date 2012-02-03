9eb6b3b657d0faac9bdecae213140b9db3933898
Tyler Cunningham <tyler@cyberchimps.com>
2012-02-02 21:00:55 -0800

    removed old option

--- includes/meta-box.php
+++ includes/meta-box.php
@@ -108,7 +108,6 @@ function initialize_the_meta_boxes() {
 			->select('page_slider_navigation_style', 'Slider Navigation Style', '', array('options' => array('Dots (default)', 'Thumbnails', 'None')) )
 			->select('page_slider_caption_style', 'Slider Caption Style', '', array('options' => array('None (default)', 'Bottom', 'Left', 'Right')) )
 			->checkbox('hide_arrows', 'Navigation Arrows', '', array('std' => 'on'))
-			->checkbox('disable_autohide', 'Navigation Arrows Autohide', '', array('std' => 'on'))
 			->checkbox('enable_wordthumb', 'WordThumb Image Resizing', '', array('std' => 'off'))
 			->sliderhelp('', 'Need Help?', '')
 		->tab("Callout Options")
