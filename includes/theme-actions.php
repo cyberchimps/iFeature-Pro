14dd838da8237427dbe462cfba7aa544d9d7c689
Tyler Cunningham <tyler@cyberchimps.com>
2012-03-02 10:36:14 -0800

    fixed links in default box content

--- includes/theme-actions.php
+++ includes/theme-actions.php
@@ -108,14 +108,14 @@ function ifeature_box_section_content() {
 			<div id="box2" class="four columns">
 				<h2 class="box-widget-title">Responsive Design</h2>
 					<img src="<?php echo $root ; ?>/images/icons/blueprint.png" height="100" alt="blueprint" class="aligncenter" />
-					<p>With Responsive Design, and <a href="http://cybersynapse.com/ifeaturepro/">iFeature Pro</a> your website will now magically adjust to mobile devices such as the iPhone, iPad, and Android devices.</p>
+					<p>With Responsive Design, and <a href="http://cyberchimps.com/ifeaturepro/">iFeature Pro</a> your website will now magically adjust to mobile devices such as the iPhone, iPad, and Android devices.</p>
 			</div><!--end box2-->
 			<?php endif; ?>
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Box Right") ) : ?>
 			<div id="box3" class="four columns">
 				<h2 class="box-widget-title">Excellent Support</h2>
 				<img src="<?php echo $root ; ?>/images/icons/docs.png" height="100" alt="docs" class="aligncenter" />
-				<p>We designed iFeature Pro to be as easy to design with as possible, if you do run into trouble we provide a <a href="http://cybersynapse.com/forum">support forum</a>, and <a href="http://www.cybersynapse.com/ifeaturepro/docs/">precise documentation</a>.</p>
+				<p>We designed iFeature Pro to be as easy to design with as possible, if you do run into trouble we provide a <a href="http://cyberchimps.com/forum">support forum</a>, and <a href="http://www.cyberchimps.com/ifeaturepro/docs/">precise documentation</a>.</p>
 			</div><!--end box3-->
 		<?php endif; ?>
 </div>
