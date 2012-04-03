0f30dd4d58038bd324a1afe82ac470076d4184f4
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-03 13:46:59 -0700

    Version #, video wrapper script, updated log.

--- core/library/js/menu.js
+++ core/library/js/menu.js
@@ -5,8 +5,3 @@ jQuery(document).ready(function($){
 jQuery(document).ready(function($){
 	$("ul").parent("li").addClass("parent"); 
 });
-
-jQuery(document).ready(function($) {
-	$("iframe[src^='http://www.youtube.com']").wrap("<div class='flex-video' />");
-	$("iframe[src^='http://player.vimeo.com']").wrap("<div class='flex-video' />");
-});
