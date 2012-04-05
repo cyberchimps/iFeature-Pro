8055fe60462f1844bf2b534e0153d3e6d9653fb5
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-05 16:09:15 -0700

    portfolio js

--- core/library/js/menu.js
+++ core/library/js/menu.js
@@ -5,3 +5,26 @@ jQuery(document).ready(function($){
 jQuery(document).ready(function($){
 	$("ul").parent("li").addClass("parent"); 
 });
+
+jQuery(document).ready(function($){
+
+	$("#gallery ul a:hover img").css("opacity", 1);
+	$("#portfolio_wrap .portfolio_caption").css("opacity", 0);
+	
+	$("#portfolio_wrap a").hover(function(){
+		$(this).children("img").fadeTo("fast", 0.3);
+		
+		$(this).children(".portfolio_caption").fadeTo("fast", 1.0);
+		
+	},function(){
+   		$(this).children("img").fadeTo("fast", 1.0);
+   		$(this).children(".portfolio_caption").fadeTo("fast", 0);
+	});
+	
+	$(".featured-image img").hover(function(){
+		$(this).fadeTo("fast", 0.75); 
+	},function(){
+   		$(this).fadeTo("fast", 1.0); 
+	});
+	
+});
