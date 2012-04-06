a2167ab6392440ddf5367ae6165002cf944f0273
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-06 14:25:23 -0700

    JS for portfolio page options

--- core/library/js/metabox-tabs.js
+++ core/library/js/metabox-tabs.js
@@ -138,10 +138,19 @@ jQuery(document).ready(function($) {
 		}
     }).trigger('change');
     
-        $('#checkbox-product_link_toggle').change(function(){
+    $('#checkbox-product_link_toggle').change(function(){
 	    var items = $("tr.product_link_url, tr.product_link_text");
 		if($(this).is(':checked')) {
 			items.show();
+		} else {
+			items.hide();
+		}
+    }).trigger('change');
+    
+	$('#checkbox-portfolio_title_toggle').change(function(){
+	    var items = $("tr.portfolio_title");
+		if($(this).is(':checked')) {
+			items.fadeIn();
 		} else {
 			items.hide();
 		}
