3c5408ce92df72f2571373f48be0a2cc6aa76c19
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-09 13:39:49 -0700

    JS for content slider meta options

--- core/library/js/jquery-custom.js
+++ core/library/js/jquery-custom.js
@@ -1,5 +1,54 @@
 jQuery(document).ready(function($) {
 	function if_check_slider_value(value) {
+		var slider_value = $(value).val();
+		
+		if ( slider_value == "0" ) {
+			$(".slider_media_type, .slider_image, .slider_url, .slider_video, .slider_text_align, .slider_caption").fadeIn();
+			
+			if_check_media_value("select[name=\'slider_media_type\']");
+
+		} else if ( slider_value == "1" ) {
+			$(".slider_media_type, .slider_image, .slider_url, .slider_video").fadeIn();
+			$(".slider_text_align, .slider_caption").hide();
+			
+			if_check_media_value("select[name=\'slider_media_type\']");
+
+		} else if ( slider_value == "2" ){
+			$(".slider_text_align, .slider_caption").fadeIn();
+			$(".slider_media_type, .slider_image, .slider_url, .slider_video").hide();
+		}	
+		return false;
+	}
+	
+	function if_check_media_value(value) {
+		var media_value = $(value).val();
+		
+		if ( media_value == "0" ) {
+			$(".slider_image, .slider_url").fadeIn();
+			$(".slider_video").hide();
+			
+		} else if ( media_value == "1" ) {
+			$(".slider_video").fadeIn();
+			$(".slider_image, .slider_url").hide();
+			
+		}
+		return false;
+	}
+
+	if_check_slider_value("select[name=\'slider_type\']");
+
+	$("select[name=\'slider_type\']").change(function() {
+		if_check_slider_value(this);
+	});
+	
+	$("select[name=\'slider_media_type\']").change(function() {
+		if_check_media_value(this);
+	});
+});
+
+
+jQuery(document).ready(function($) {
+	function if_check_slider_value(value) {
 		var slider_value = $("select[name=\'page_slider_type\']").val();
 
 		if ( slider_value == "0" ) {
