29b7db73d811e1937949c85296aee6edc2a03221
hugosaner <hugo@sanerdesign.com>
2012-08-13 06:25:12 +0100

    $plugin_name in functions.php
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- functions.php
+++ functions.php
@@ -614,6 +614,7 @@ function presstrends() {
 // Add your PressTrends and Theme API Keys
 $api_key = 'zwhgyc1lnt56hki8cpwobb47bblas4er226b';
 $auth = 'c3kal5v4c3o30aiy9kewst9a8vsuatlll';
+$plugin_name = '';
 
 // NO NEED TO EDIT BELOW
 $data = get_transient( 'presstrends_data' );
