c69235cf1db5b06ca1b80303a7e932b459d5de42
hugosaner <hugo@sanerdesign.com>
2012-09-10 11:31:38 +0100

    Custom background being selected but not set left empty css calls. Somehow these have been affecting Woo Commerce baskets. Have set Queries to check if they have been set
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- includes/options-functions.php
+++ includes/options-functions.php
@@ -151,11 +151,17 @@ function background_option() {
 	}
 	
 	if ($options->get($themeslug.'_custom_background') == "1") {
-	
-		echo '<style type="text/css">';
-		echo "body {background-image: url('$custom'); background-color: $color; background-repeat: $repeat; background-position: $position; background-attachment: $attachment;}";
-		echo '</style>';
-	
+		$color = (!empty($color)) ? $color : '#ffffff';
+		if(!empty($custom)){
+			echo '<style type="text/css">';
+			echo "body {background-image: url('$custom'); background-color: $color; background-repeat: $repeat; background-position: $position; background-attachment: $attachment;}";
+			echo '</style>';
+		}
+		else {
+			echo '<style type="text/css">';
+			echo "body {background-color: $color;}";
+			echo '</style>';
+		}
 	}
 	
 }
