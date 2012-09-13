09bda83b6728de9f3ba33e52d4f1c19d2048f932
hugosaner <hugo@sanerdesign.com>
2012-09-13 11:37:31 +0100

    Moved callout styles to head using an add action. Also stopped the callout from using pie to see if this prevents the ie js issues we have been having where plugins don't play nice with our templates in ie and the display drops out.
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/pro/actions/callout-actions.php
+++ core/pro/actions/callout-actions.php
@@ -73,54 +73,6 @@ function synapse_callout_section_content() {
 	
 /* End variable definition. */	
 
-/* Echo custom button color. */
-
-	if ($bcolor != "") {
-		echo '<style type="text/css" media="screen">';
-		echo "#calloutbutton {background: $bcolor ;}";
-		echo '</style>';
-	}
-	
-/* End custom button color. */
-
-/* Echo custom button text color. */
-
-	if ($bcolor != "") {
-		echo '<style type="text/css" media="screen">';
-		echo "#calloutbutton a {color: $btcolor ;}";
-		echo '</style>';
-	}
-	
-/* End custom button color. */
-
-/* Echo custom text color. */
-
-	if ($tcolor != "") {
-		echo '<style type="text/css" media="screen">';
-		echo "#callout_text {color: $tcolor ;}";
-		echo '</style>';
-	}
-	
-/* Echo custom title color. */
-
-	if ($ticolor != "") {
-		echo '<style type="text/css" media="screen">';
-		echo ".callout_title {color: $ticolor ;}";
-		echo '</style>';
-	}
-
-/* End custom text color. */
-
-/* Echo background color CSS. */	
-
-	if ($customcalloutbgcolor != ''){
-		echo '<style type="text/css" media="screen">';
-		echo "#calloutwrap {background: $customcalloutbgcolor ;}";
-		echo '</style>';
-	}
-		
-/* End CSS. */	
-
 /* Define Callout title. */	
 
 	if ($title == '') {
@@ -202,8 +154,92 @@ function synapse_callout_section_content() {
 </div>
 
 <?php
-	
 }
+
+function synapse_callout_section_css() {
+	global $options, $themeslug, $post; //call globals
+	$root = get_template_directory_uri();  
+
+/* Define variables. */	
+if( $options->get($themeslug.'_blog_custom_callout_options') ) {
+	if (is_page()) {
+		$callout = get_post_meta($post->ID, 'enable_callout_section' , true);
+		$calloutbgcolor = get_post_meta($post->ID, 'callout_background_color' , true);
+		$bcolor = get_post_meta($post->ID, 'custom_callout_button_color' , true);
+		$btcolor = get_post_meta($post->ID, 'custom_callout_button_text_color' , true);
+		$tcolor = get_post_meta($post->ID, 'custom_callout_text_color' , true);
+		$ticolor = get_post_meta($post->ID, 'custom_callout_title_color' , true);
+		$title = get_post_meta($post->ID, 'callout_title' , true);
+		$text = get_post_meta($post->ID, 'callout_text' , true);
+		$btext = get_post_meta($post->ID, 'callout_button_text' , true);
+		$link = get_post_meta($post->ID, 'callout_url' , true);
+		$image = get_post_meta($post->ID, 'callout_image' , true);
+		$hidebutton = get_post_meta($post->ID, 'disable_callout_button' , true);
+		$customcalloutbgcolor = get_post_meta($post->ID, 'custom_callout_color' , true);
+	}
+	
+	else {
+		$calloutbgcolor = $options->get($themeslug.'_blog_callout_bg_color');
+		$bcolor = $options->get($themeslug.'_blog_callout_button_color');
+		$btcolor = $options->get($themeslug.'_blog_callout_button_text_color');
+		$tcolor = $options->get($themeslug.'_blog_callout_text_color');
+		$ticolor = $options->get($themeslug.'_blog_callout_title_color');
+		$title = $options->get($themeslug.'_blog_callout_title');
+		$text = $options->get($themeslug.'_blog_callout_text');
+		$btext = $options->get($themeslug.'_blog_callout_button_text');
+		$link = $options->get($themeslug.'_blog_callout_button_url');
+		$imagesrc = $options->get($themeslug.'_blog_custom_callout_button');
+		$image = $imagesrc['url'];
+		$hidebutton = $options->get($themeslug.'_blog_callout_button');
+		$customcalloutbgcolor = $options->get($themeslug.'_blog_callout_bg_color');
+	}
+		
+/* End variable definition. */	
+/* Start CSS */
+
+echo '<style type="text/css" media="screen">';
+
+/* Echo custom button color. */
+	if ($bcolor != "") {
+		echo "#calloutbutton {background: none; background-color: $bcolor ;}";
+	}
+	
+/* End custom button color. */
+
+/* Echo custom button text color. */
+
+	if ($bcolor != "") {
+		echo "#calloutbutton a {color: $btcolor ;}";
+	}
+	
+/* End custom button color. */
+
+/* Echo custom text color. */
+
+	if ($tcolor != "") {
+		echo "#callout_text {color: $tcolor ;}";
+	}
+	
+/* Echo custom title color. */
+
+	if ($ticolor != "") {
+		echo ".callout_title {color: $ticolor ;}";
+	}
+
+/* End custom text color. */
+
+/* Echo background color CSS. */	
+
+	if ($customcalloutbgcolor != ''){
+		echo "#calloutwrap {background: none; background-color: $customcalloutbgcolor ;}";
+	}
+echo '</style>';
+}
+/* End CSS. */	
+}
+
+add_action( 'wp_head', 'synapse_callout_section_css' );
+
 
 /**
 * End
