a1823a6f1599c5bdca339cbcd166cdd0ab27f61f
hugosaner <hugo@sanerdesign.com>
2012-10-09 17:31:30 +0100

    Quick Edit error - fixed issue by checing if we are on the edit.php page first. If not the save should get handled by the WP ajax
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/metabox/meta-box-class.php
+++ core/metabox/meta-box-class.php
@@ -584,6 +584,10 @@ class RW_Meta_Box {
 
 	// Save data from meta box
 	function save($post_id) {
+		global $pagenow;
+		
+		// check that the save is coming from the edit post page and not the quick edit
+    if( 'post.php' == $pagenow ) {
 
 		if (isset($_POST['post_type'])) {
 			$post_type = $_POST['post_type'];
@@ -622,6 +626,7 @@ class RW_Meta_Box {
 					$this->save_field($post_id, $field, $old, $new);
 				}
 			}
+		}
 		}
 	}
 
